1d67fdb3a2cd6b6d2a4bf59de7798adb2a632bd4
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-01-28 13:57:53 +0300

    Add `css` command to build with tailwind.

--- melody/app/tailwind.config.js
+++ melody/app/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
 
 module.exports = {
     content: ["./index.html", "./src/App.vue", "./src/components/*.vue", "./src/views/*.vue"],
