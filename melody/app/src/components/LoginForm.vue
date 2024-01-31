cd2e73046ebb07b71f4a85ed85d1fc2f07bcbb57
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-01-31 22:42:05 +0300

    Add non-required code input.

--- melody/app/src/components/LoginForm.vue
+++ melody/app/src/components/LoginForm.vue
@@ -16,6 +16,10 @@
               <label for="password" class="block mb-2 text-neutral-900 dark:text-neutral-50">Password</label>
               <input type="password" name="password" v-model="userData.password" class="bg-neutral-50 border border-neutral-300 text-neutral-900 sm:text-sm font-mono rounded-lg block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-neutral-50 focus:outline-none" placeholder="••••••••" required>
             </div>
+            <div>
+              <label for="code" class="block mb-2 text-neutral-900 dark:text-neutral-50">Code</label>
+              <input type="text" name="code" v-model="userData.code" class="bg-neutral-50 border border-neutral-300 text-neutral-900 sm:text-sm font-mono rounded-lg block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-neutral-50 focus:outline-none" placeholder="000000">
+            </div>
             <div class="flex items-center justify-between">
               <router-link class="text-neutral-600 hover:underline dark:text-neutral-500" to="/forgot">Forgot password?</router-link>
             </div>
