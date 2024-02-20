409bde99bfa3c00619887b2f1e6b0beb2b176fa1
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-02-20 22:25:08 +0300

    Show login form in authorization.

--- melody/app/src/views/AuthorizeView.vue
+++ melody/app/src/views/AuthorizeView.vue
@@ -1,7 +1,10 @@
 <template>
-  <AuthorizeForm/>
+  <AuthorizeForm v-if="isAuthorized()"/>
+  <LoginForm v-else/>
 </template>
 
 <script setup lang="ts">
+import { isAuthorized } from "@/checks";
 import AuthorizeForm from "@/components/AuthorizeForm.vue";
+import LoginForm from "@/components/LoginForm.vue";
 </script>
